cd4e5f1e7c1167e0db0d9515891b5443e3f23068
shawnkoon <shawn9405@gmail.com>
2016-11-21 14:37:46 -0800

    Added 3 threads testing application.

---
+++ parallel_proces_tester/ParallelObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace parallel_proces_tester
+{
+    public class ParallelObject
+    {
+        private int ID;
+
+        public ParallelObject(int ID)
+        {
+            this.ID = ID;
+        }
+
+        public void Print()
+        {
+            for(int i = 0; i < 100; i++)
+                Console.WriteLine("I am obj ID=" + this.ID + " i="+i);
+        }
+    }
+}
--- parallel_proces_tester/Program.cs
+++ parallel_proces_tester/Program.cs
@@ -9,6 +9,13 @@ namespace parallel_proces_tester
     {
         public static void Main(string[] args)
         {
+            List<ParallelObject> listParallels = new List<ParallelObject>();
+            listParallels.Add(new ParallelObject(1));
+            listParallels.Add(new ParallelObject(2));
+            listParallels.Add(new ParallelObject(3));
+            Parallel.ForEach(listParallels, (curParallels) => curParallels.Print());
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadKey();
         }
     }
 }
